4cfc448cac0189c959a03d55c8cb71bc3702a108
mohsin-8 <mohsinalikhan518@gmail.com>
2022-01-29 22:48:26 +0500

    intro to reactjs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World App</h1>
+      <div className="data">
+        <h3>Name: Mohsin Ali Khan</h3>
+        <h3>Course: Web & Mobile Hybrid Application</h3>
+        <h3>Institute: JAWAN PAKISTAN</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
